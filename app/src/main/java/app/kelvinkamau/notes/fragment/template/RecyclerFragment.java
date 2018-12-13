8836dcbe8d60de05f67f7413ca7e6f21a3097a53
Kelvin Kamau <kelvinkamau6808@gmail.com>
2018-12-13 21:33:14 +0300

    updated fonts

--- app/src/main/java/app/kelvinkamau/notes/fragment/template/RecyclerFragment.java
+++ app/src/main/java/app/kelvinkamau/notes/fragment/template/RecyclerFragment.java
@@ -108,203 +108,208 @@ abstract public class RecyclerFragment<T extends DatabaseModel, A extends ModelA
 
                                 StringBuilder message = new StringBuilder();
                                 message.append(length).append(" ").append(getItemName());
-                                if (length > 1) message.append("s were deleted");
-                                else message.append(" was deleted.");
+                                if (length > 1) {
+                                    message.setLength(message.length() - 1);
+                                    message.append("ies were deleted");
+                                }
+                                else{
+                                        message.append(" was deleted.");
+                                    }
 
-                                Snackbar.make(fab != null ? fab : selectionToolbar, message.toString(), 7000)
-                                        .setAction(R.string.undo, new View.OnClickListener() {
-                                            @Override
-                                            public void onClick(View view) {
-                                                new Thread() {
+                                        Snackbar.make(fab != null ? fab : selectionToolbar, message.toString(), 7000)
+                                                .setAction(R.string.undo, new View.OnClickListener() {
                                                     @Override
-                                                    public void run() {
-                                                        Controller.instance.undoDeletion();
-                                                        if (categoryId != DatabaseModel.NEW_MODEL_ID) {
-                                                            Controller.instance.addCategoryCounter(categoryId, length);
-                                                        }
-
-                                                        Collections.sort(undos, new Comparator<T>() {
-                                                            @Override
-                                                            public int compare(T t1, T t2) {
-                                                                if (t1.position < t2.position)
-                                                                    return -1;
-                                                                if (t1.position == t2.position)
-                                                                    return 0;
-                                                                return 1;
-                                                            }
-                                                        });
-
-                                                        getActivity().runOnUiThread(new Runnable() {
+                                                    public void onClick(View view) {
+                                                        new Thread() {
                                                             @Override
                                                             public void run() {
-                                                                for (int i = 0; i < length; i++) {
-                                                                    T item = undos.get(i);
-                                                                    addItem(item, item.position);
+                                                                Controller.instance.undoDeletion();
+                                                                if (categoryId != DatabaseModel.NEW_MODEL_ID) {
+                                                                    Controller.instance.addCategoryCounter(categoryId, length);
                                                                 }
-                                                            }
-                                                        });
-                                                        interrupt();
-                                                    }
-                                                }.start();
-                                            }
-                                        })
-                                        .show();
-                            }
-                        });
 
-                        interrupt();
+                                                                Collections.sort(undos, new Comparator<T>() {
+                                                                    @Override
+                                                                    public int compare(T t1, T t2) {
+                                                                        if (t1.position < t2.position)
+                                                                            return -1;
+                                                                        if (t1.position == t2.position)
+                                                                            return 0;
+                                                                        return 1;
+                                                                    }
+                                                                });
+
+                                                                getActivity().runOnUiThread(new Runnable() {
+                                                                    @Override
+                                                                    public void run() {
+                                                                        for (int i = 0; i < length; i++) {
+                                                                            T item = undos.get(i);
+                                                                            addItem(item, item.position);
+                                                                        }
+                                                                    }
+                                                                });
+                                                                interrupt();
+                                                            }
+                                                        }.start();
+                                                    }
+                                                })
+                                                .show();
+                                    }
+                                });
+
+                                interrupt();
+                            }
+                        }.start();
+                    }
+                });
+
+                if (fab != null) {
+                    fab.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            onClickFab();
+                        }
+                    });
+                }
+
+                Intent data = getActivity().getIntent();
+                if (data != null) {
+                    // Get the parent data
+                    categoryId = data.getLongExtra(OpenHelper.COLUMN_ID, DatabaseModel.NEW_MODEL_ID);
+                    categoryTitle = data.getStringExtra(OpenHelper.COLUMN_TITLE);
+                    categoryTheme = data.getIntExtra(OpenHelper.COLUMN_THEME, Category.THEME_GREEN);
+                    categoryPosition = data.getIntExtra("position", 0);
+
+                    if (categoryTitle != null) {
+                        ((TextView) getActivity().findViewById(R.id.title)).setText(categoryTitle);
+                    }
+                }
+
+                loadItems();
+            }
+
+            public void onChangeCounter(int count) {
+                selectionCounter.setText(String.format(Locale.US, "%d", count));
+            }
+
+            public void toggleSelection(boolean state) {
+                selectionState = state;
+                activity.onChangeSelection(state);
+                if (state) {
+                    Animator.create(getContext())
+                            .on(selectionToolbar)
+                            .setStartVisibility(View.VISIBLE)
+                            .animate(R.anim.fade_in);
+                } else {
+                    Animator.create(getContext())
+                            .on(selectionToolbar)
+                            .setEndVisibility(View.GONE)
+                            .animate(R.anim.fade_out);
+
+                    deselectAll();
+                }
+            }
+
+            private void deselectAll() {
+                while (!selected.isEmpty()) {
+                    adapter.notifyItemChanged(items.indexOf(selected.remove(0)));
+                }
+            }
+
+            public void loadItems() {
+                new Thread() {
+                    @SuppressWarnings("unchecked")
+                    @Override
+                    public void run() {
+                        try {
+                            if (categoryId == DatabaseModel.NEW_MODEL_ID) {
+                                // Get all categories
+                                items = (ArrayList<T>) Category.all();
+                            } else {
+                                // Get notes of the category by categoryId
+                                items = (ArrayList<T>) Note.all(categoryId);
+                            }
+
+                            adapter = getAdapterClass().getDeclaredConstructor(
+                                    ArrayList.class,
+                                    ArrayList.class,
+                                    ModelAdapter.ClickListener.class
+                            ).newInstance(items, selected, getListener());
+
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    toggleEmpty();
+
+                                    recyclerView.setAdapter(adapter);
+                                    recyclerView.setLayoutManager(new LinearLayoutManager(
+                                            getContext(),
+                                            LinearLayoutManager.VERTICAL,
+                                            false
+                                    ));
+                                }
+                            });
+                        } catch (Exception ignored) {
+                        } finally {
+                            interrupt();
+                        }
                     }
                 }.start();
             }
-        });
 
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    onClickFab();
+            private void toggleEmpty() {
+                if (items.isEmpty()) {
+                    empty.setVisibility(View.VISIBLE);
+                    recyclerView.setVisibility(View.GONE);
+                } else {
+                    empty.setVisibility(View.GONE);
+                    recyclerView.setVisibility(View.VISIBLE);
                 }
-            });
-        }
-
-        Intent data = getActivity().getIntent();
-        if (data != null) {
-            // Get the parent data
-            categoryId = data.getLongExtra(OpenHelper.COLUMN_ID, DatabaseModel.NEW_MODEL_ID);
-            categoryTitle = data.getStringExtra(OpenHelper.COLUMN_TITLE);
-            categoryTheme = data.getIntExtra(OpenHelper.COLUMN_THEME, Category.THEME_GREEN);
-            categoryPosition = data.getIntExtra("position", 0);
-
-            if (categoryTitle != null) {
-                ((TextView) getActivity().findViewById(R.id.title)).setText(categoryTitle);
             }
-        }
 
-        loadItems();
-    }
+            public void refreshItem(int position) {
+                adapter.notifyItemChanged(position);
+            }
 
-    public void onChangeCounter(int count) {
-        selectionCounter.setText(String.format(Locale.US, "%d", count));
-    }
+            public T deleteItem(int position) {
+                T item = items.remove(position);
+                adapter.notifyItemRemoved(position);
+                toggleEmpty();
+                return item;
+            }
 
-    public void toggleSelection(boolean state) {
-        selectionState = state;
-        activity.onChangeSelection(state);
-        if (state) {
-            Animator.create(getContext())
-                    .on(selectionToolbar)
-                    .setStartVisibility(View.VISIBLE)
-                    .animate(R.anim.fade_in);
-        } else {
-            Animator.create(getContext())
-                    .on(selectionToolbar)
-                    .setEndVisibility(View.GONE)
-                    .animate(R.anim.fade_out);
+            public void addItem(T item, int position) {
+                if (items.isEmpty()) {
+                    empty.setVisibility(View.GONE);
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
 
-            deselectAll();
-        }
-    }
+                items.add(position, item);
+                adapter.notifyItemInserted(position);
+            }
 
-    private void deselectAll() {
-        while (!selected.isEmpty()) {
-            adapter.notifyItemChanged(items.indexOf(selected.remove(0)));
-        }
-    }
-
-    public void loadItems() {
-        new Thread() {
-            @SuppressWarnings("unchecked")
             @Override
-            public void run() {
-                try {
-                    if (categoryId == DatabaseModel.NEW_MODEL_ID) {
-                        // Get all categories
-                        items = (ArrayList<T>) Category.all();
-                    } else {
-                        // Get notes of the category by categoryId
-                        items = (ArrayList<T>) Note.all(categoryId);
-                    }
-
-                    adapter = getAdapterClass().getDeclaredConstructor(
-                            ArrayList.class,
-                            ArrayList.class,
-                            ModelAdapter.ClickListener.class
-                    ).newInstance(items, selected, getListener());
-
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            toggleEmpty();
-
-                            recyclerView.setAdapter(adapter);
-                            recyclerView.setLayoutManager(new LinearLayoutManager(
-                                    getContext(),
-                                    LinearLayoutManager.VERTICAL,
-                                    false
-                            ));
-                        }
-                    });
-                } catch (Exception ignored) {
-                } finally {
-                    interrupt();
-                }
+            public void onAttach(Context context) {
+                super.onAttach(context);
+                this.activity = (Callbacks) context;
             }
-        }.start();
-    }
 
-    private void toggleEmpty() {
-        if (items.isEmpty()) {
-            empty.setVisibility(View.VISIBLE);
-            recyclerView.setVisibility(View.GONE);
-        } else {
-            empty.setVisibility(View.GONE);
-            recyclerView.setVisibility(View.VISIBLE);
+            public void init(View view) {
+            }
+
+            public abstract void onClickFab();
+
+            public abstract int getLayout();
+
+            public abstract String getItemName();
+
+            public abstract Class<A> getAdapterClass();
+
+            public abstract ModelAdapter.ClickListener getListener();
+
+            public interface Callbacks {
+                void onChangeSelection(boolean state);
+
+                void toggleOneSelection(boolean state);
+            }
         }
-    }
-
-    public void refreshItem(int position) {
-        adapter.notifyItemChanged(position);
-    }
-
-    public T deleteItem(int position) {
-        T item = items.remove(position);
-        adapter.notifyItemRemoved(position);
-        toggleEmpty();
-        return item;
-    }
-
-    public void addItem(T item, int position) {
-        if (items.isEmpty()) {
-            empty.setVisibility(View.GONE);
-            recyclerView.setVisibility(View.VISIBLE);
-        }
-
-        items.add(position, item);
-        adapter.notifyItemInserted(position);
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        this.activity = (Callbacks) context;
-    }
-
-    public void init(View view) {
-    }
-
-    public abstract void onClickFab();
-
-    public abstract int getLayout();
-
-    public abstract String getItemName();
-
-    public abstract Class<A> getAdapterClass();
-
-    public abstract ModelAdapter.ClickListener getListener();
-
-    public interface Callbacks {
-        void onChangeSelection(boolean state);
-
-        void toggleOneSelection(boolean state);
-    }
-}
